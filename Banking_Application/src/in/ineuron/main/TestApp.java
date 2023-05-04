6073c885c1c6f72cdd16158c8fbd5a0eff5885b5
Sanketgadakh66 <sanketgadakh66@gmail.com>
2023-05-04 22:21:48 +0530

    First Commit

---
+++ Banking_Application/src/in/ineuron/main/TestApp.java
@@ -0,0 +1,12 @@
+package in.ineuron.main;
+
+import in.ineuron.bank.Bank;
+
+public class TestApp {
+
+	public static void main(String[] args) {
+		Bank bank = new Bank();
+		bank.ShowMenu();
+	}
+
+}
